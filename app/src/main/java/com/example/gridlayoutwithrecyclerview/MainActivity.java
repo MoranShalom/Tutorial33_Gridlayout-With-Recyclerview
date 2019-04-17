1d60e43e980b376d2e58cabee37987f12c2fc5a8
MoranShalom <moranshalom1234@gmail.com>
2019-04-17 13:29:41 +0300

    update

--- app/src/main/java/com/example/gridlayoutwithrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/gridlayoutwithrecyclerview/MainActivity.java
@@ -15,17 +15,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView mRecyclerView = findViewById(R.id.recyclerView);
+        //declare and initialize Android Recyclerview.
+         mRecyclerView = findViewById(R.id.recyclerView);
 
+        /**   -- GridLayoutManager ---
+         *  declare and initialize Android GridLayoutManager, this will be used to
+         *  determine how you want to arrange the images inside Recyclerview.
+         *  Android GridLayoutManager takes 2 argument: First one is the context,
+         *  the second one is used to indicate the number of columns that you want to show inside Recyclerview.
+         */
         GridLayoutManager mGridLayoutManager = new GridLayoutManager(MainActivity.this, 2);
         mRecyclerView.setLayoutManager(mGridLayoutManager);
-        mRecyclerView.getLayoutManager().setMeasurementCacheEnabled(false);
 
+
+
+        //Next you will need to declare and initialize the images array that you will
+        // have to use it later inside (MyAdapter).
         mflowersList = new int[]{R.drawable.image1, R.drawable.image2, R.drawable.image3,
                 R.drawable.image4, R.drawable.image5, R.drawable.image6, R.drawable.image7,
                 R.drawable.image8, R.drawable.image9, R.drawable.image5, R.drawable.image3,
                 R.drawable.image1, R.drawable.image6, R.drawable.image8};
 
+
+        //The only thing remaining before the app is ready is you will need to declare
+        // and initialize (MyAdapter) and make sure to use this adapter with Android Recyclerview.
         MyAdapter myAdapter = new MyAdapter(MainActivity.this, mflowersList);
         mRecyclerView.setAdapter(myAdapter);
     }
--- app/src/main/java/com/example/gridlayoutwithrecyclerview/MyAdapter.java
+++ app/src/main/java/com/example/gridlayoutwithrecyclerview/MyAdapter.java
@@ -1,5 +1,12 @@
 package com.example.gridlayoutwithrecyclerview;
 
+/**
+ * Create a new java class and name it (MyAdapter) and make it extends RecyclerView.Adapter.
+ * Make sure to implement Recyclerview required methods (onCreateViewHolder(), onBindViewHolder(),
+ * and getItemCount()), based on the error that you see below PlaceViewHolder you need
+ * to create a new class called (PlaceViewHolder).
+ */
+
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
@@ -10,6 +17,8 @@ import android.widget.ImageView;
 
 public class MyAdapter extends RecyclerView.Adapter <PlaceViewHolder>{
 
+    //Inside MyAdapter.java class you need to declare 2 variables and a constructor,
+    // this constructor will be used to pass data from MainActivity.java to this adapter.
     private Context mContext;
     private int[] mflowersList;
 
@@ -21,6 +30,9 @@ public class MyAdapter extends RecyclerView.Adapter <PlaceViewHolder>{
 
     @Override
     public PlaceViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        //Inside Recyclerview (onCreateViewHolder) method you need to inflate
+        // the custom layout file recyclerview_custom_layout.xml.
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_custom_layout,
                 parent, false);
         return new PlaceViewHolder(view);
@@ -28,6 +40,8 @@ public class MyAdapter extends RecyclerView.Adapter <PlaceViewHolder>{
 
     @Override
     public void onBindViewHolder(final PlaceViewHolder holder, int position) {
+
+        // Inside (onBindViewHolder) is where you will initialize (ivPlace).
         holder.mflowers.setImageResource(mflowersList[position]);
 
         holder.mflowers.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +58,9 @@ public class MyAdapter extends RecyclerView.Adapter <PlaceViewHolder>{
 
     @Override
     public int getItemCount() {
+
+        //Finally you need to change the return value inside (getItemCount())
+        // to actually return the size of the images array instead of null value.
         return mflowersList.length;
     }
 }
@@ -52,8 +69,12 @@ class PlaceViewHolder extends RecyclerView.ViewHolder {
 
     ImageView mflowers;
 
+    //Inside PlaceViewHolder class and based on the error you will need to add a default constructor.
     public PlaceViewHolder(View itemView) {
         super(itemView);
+
+        //Inside the constructor of PlaceViewHolder class you need to declare and
+        // initialize Android ImageView (flowers).
          mflowers = itemView.findViewById(R.id.flowers);
     }
 }
